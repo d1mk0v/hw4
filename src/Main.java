ca20769b1e1512917caca9be1e5b38a3587c08e4
d1mk0v <79874763588@ya.ru>
2023-04-22 16:40:27 +0500

    Задача 4

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
 
     }
     public static void task1() {
@@ -36,5 +37,27 @@ public class Main {
         }
         else
             System.out.println("Если скорость " + speed + " , то можно ездить спокойно");
+    }
+    public static void  task4() {
+        System.out.println("Задача 4");
+        double age = 65;
+        if (age < 2){
+            System.out.println("Если возраст человека равен " + age + ", то ему надо идти спать");
+        }
+        if (age >= 2 && age <=6) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад");
+        }
+        if (age > 6 && age <= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в школу");
+        }
+        if (age > 18 && age <= 24) {
+            System.out.println("Если возраст человека равен " + age + ", то его место в университете");
+        }
+        if (age > 24 && age <= 60) {
+            System.out.println("Если возраст человека равен " + age + ", то ему пора ходить на работу");
+        }
+        if (age > 60) {
+            System.out.println("Если возраст человека равен " + age + ", то он может отдохнуть");
+        }
     }
 }
