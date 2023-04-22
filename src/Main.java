6ad60c55f13115921a5e21a1764df5d456c1c3f9
d1mk0v <79874763588@ya.ru>
2023-04-22 15:16:57 +0500

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + " , то он совершеннолетний");
+        }
+        if (age < 18){
+            System.out.println("Если возраст человека равен " + age + " , то он не достиг совершенолетия, нужно немного подождать");
+        }
+    }
+}
